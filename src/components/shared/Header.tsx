e8b3ef1252185ec10dfb45211707a5536216bf13
masudrahman-dev <masudrahman.dev@gmail>
2023-07-23 04:30:33 +0600

    Header makes done

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,11 +1,66 @@
-
-
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+const navItems = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Colleges",
+    path: "/colleges",
+  },
+  {
+    label: "Admission",
+    path: "/admission",
+  },
+  {
+    label: "My College",
+    path: "/my-college",
+  },
+];
 const Header = () => {
-    return (
-        <div>
-            hearer
+  const items = navItems.map((item) => (
+    <MenuItem as={Link} to={item.path} key={item.label}>
+      {item.label}
+    </MenuItem>
+  ));
+
+  return (
+    <div>
+      <nav className="bg-blue-500 ">
+        <div className="p-4 max-w-screen-xl mx-auto">
+          <div className="flex justify-between items-center">
+            <Link
+              to={"/"}
+              className="  border p-3 rounded text-white font-semibold text-lg "
+            >
+              <h1>CollegeBooking</h1>
+            </Link>
+
+            <div>
+              <ul className=" hidden md:flex gap-5 items-center text-white font-semibold text-lg">
+                {/* {items} */}
+              </ul>
+              <Menu>
+                <MenuButton>Open menu</MenuButton>
+                <MenuList>{items}</MenuList>
+              </Menu>
+            </div>
+          </div>
         </div>
-    );
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
